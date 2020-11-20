181af1431b0b757f0bde0bd461c1e949464b0e7e
Nijesh W <nijesh.w.17@gmail.com>
2020-11-20 18:29:27 +0530

    Fix: Conflict fixed

--- server/routes.ts
+++ server/routes.ts
@@ -4,12 +4,7 @@ import tenantsRouter from './api/controllers/tenants/router';
 import authRouter from './api/controllers/auth/router';
 
 export default function routes(app: Application): void {
-<<<<<<< HEAD
-  app.use('/api/v1/users', usersRouter);
+  app.use('/users', usersRouter);
   app.use('/tenants/', tenantsRouter);
   app.use('/auth/', authRouter);
-=======
-  app.use('/v1/users', usersRouter);
-  app.use('/v1/tenants/', tenantsRouter);
->>>>>>> 993bf0c61200d62871f3a2ada948efd3f7b138e1
 }
