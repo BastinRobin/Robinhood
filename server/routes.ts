dc73dc2efa5c0d34b56acf6318b733bac16d016e
Nijesh W <nijesh.w.17@gmail.com>
2020-12-22 14:04:48 +0530

    Fix: Router bug fix

--- server/routes.ts
+++ server/routes.ts
@@ -3,10 +3,8 @@ import usersRouter from './api/controllers/users/router';
 import tenantsRouter from './api/controllers/tenants/router';
 import authRouter from './api/controllers/auth/router';
 
-import profiletypeRouter from './api/controllers/profiletype/router';
 export default function routes(app: Application): void {
   app.use('/v1/users', usersRouter);
   app.use('/v1/tenants/', tenantsRouter);
   app.use('/v1/auth/', authRouter);
-  app.use('/v1/profiletype/', profiletypeRouter);
 }
