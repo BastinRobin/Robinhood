2f8d3973dc035c5077c21be20ce9cd19611fdd14
Bastin Robins .J <bastinrobins@gmail.com>
2021-01-02 00:08:16 +0530

    Initial Tagged commit

--- server/api/controllers/upload/router.ts
+++
@@ -1,4 +0,0 @@
-import express from 'express';
-import controller from './controller';
-import tokenVerify from './../../middlewares/token.verify';
-export default express.Router().post('/', tokenVerify, controller.upload);
